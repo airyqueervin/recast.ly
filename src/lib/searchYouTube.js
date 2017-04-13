c777c807cfeb241f22b3e984a69ef19146f7ef65
Hack Reactor Student <student@hackreactor.com>
2017-04-12 21:15:33 -0700

    Add partially functioning search youTube helper function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,17 @@
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: { q: options.query, maxResults: options.max, key: options.key},
+    success: function(data) {
+      console.log('SUCCEDED TO GET DATA', data);
+      callback(data);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });  
 };
 
 window.searchYouTube = searchYouTube;
