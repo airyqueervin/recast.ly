fd7e3512f075fa35279f432e72243fcda6b5a5ae
Hack Reactor Student <student@hackreactor.com>
2017-04-12 21:41:31 -0700

    add fully functioning searchYouTube helper function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -3,10 +3,10 @@ var searchYouTube = (options, callback) => {
   $.ajax({
     type: 'GET',
     url: 'https://www.googleapis.com/youtube/v3/search',
-    data: { q: options.query, maxResults: options.max, key: options.key},
+    data: {part: 'snippet', q: options.query, maxResults: options.max, key: options.key, type: 'video', videoEmbeddable: true},
     success: function(data) {
       console.log('SUCCEDED TO GET DATA', data);
-      callback(data);
+      callback(data.items);
     },
     error: function(error) {
       console.log(error);
